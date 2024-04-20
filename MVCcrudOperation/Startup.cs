449f5a770f4cddef87a35a471b4e57d1bc865be0
lenovo <lenovo@DESKTOP-OGNHHM0>
2024-04-20 20:25:44 +0530

    Add project files.

---
+++ MVCcrudOperation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCcrudOperation.Startup))]
+namespace MVCcrudOperation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
